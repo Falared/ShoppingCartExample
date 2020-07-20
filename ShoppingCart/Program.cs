ede687bf67a38eb26667712837e20c0607376dbe
EricRedwine <EricRedwine@180medical.com>
2020-07-20 17:40:51 -0500

    Removed debug tests

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -8,12 +8,7 @@ namespace ShoppingCart
     {
         static void Main(string[] args)
         {
-            PointOfSaleService pointOfSaleService = new PointOfSaleService();
-            pointOfSaleService.Scan("A");
-            pointOfSaleService.Scan("A");
-            pointOfSaleService.Scan("B");
-            Console.WriteLine(pointOfSaleService.Total().ToString());
-
+            
         }
 
        
